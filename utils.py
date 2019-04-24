b4db2d13caa84b1229b3c1adfe240f3845ee5908
Matt Butterfield <matt.butterfield@mavenclinic.com>
2019-04-24 08:41:14 -0400

    update utils

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import os
 
+from jinja2 import Template
+
 import praw
 
 
@@ -9,9 +11,34 @@ REDDIT_USERNAME = os.environ.get('REDDIT_USERNAME')
 REDDIT_PASSWORD = os.environ.get('REDDIT_PASSWORD')
 
 
-def get_reddit():
+def send_emails():
+    reddit = _reddit()
+    subreddits = []
+    template_data = _template_data()
+    for name in SUBREDDITS:
+        posts = []
+        for result in reddit.subreddit(name).top('day', limit=10):
+            posts.append({
+                'title': result.title,
+                'url': result.url,
+            })
+        subreddits.append({'name': name, 'posts': posts})
+
+    with open('scratch.html', 'w') as fp:
+        fp.write(Template(template_data).render(
+            email_management_url='',
+            subreddits=subreddits,
+        ))
+
+
+def _reddit():
     return praw.Reddit(client_id=REDDIT_CLIENT_ID,
                        client_secret=REDDIT_CLIENT_SECRET,
                        username=REDDIT_USERNAME,
                        password=REDDIT_PASSWORD,
-                       user_agent='summer by /u/deneb150')
+                       user_agent='orangered by /u/deneb150')
+
+
+def _template_data():
+    with open('templates/email.html') as fp:
+        return fp.read()
