91f5b6d98f50efd9c8228e4f4ee97edc30d71406
Matt Butterfield <matt.butterfield@mavenclinic.com>
2019-07-04 15:16:42 -0400

    subreddits -> account

--- static/js/manage.js
+++ static/js/manage.js
@@ -30,7 +30,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Success! Subreddits updated.</strong>");
+            .append("<strong>Success! Account updated.</strong>");
           $('#success > .alert-success')
             .append('</div>');
         },
