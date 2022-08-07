72bc4cd01fd5308a0ef1e635f63a699d9ccd52ec
Matt Butterfield <matt@mattbutterfield.com>
2022-08-07 15:38:41 -0400

    everything but the form

--- static/ts/features/SignupForm/SignupForm.tsx
+++ static/ts/features/SignupForm/SignupForm.tsx
@@ -5,7 +5,8 @@ import {
   updateEmail,
   selectEmail,
 } from "features/SignupForm/signupFormSlice";
-import {AppBar, Button, Link, Toolbar, Typography} from "@mui/material";
+import {AppBar, Button, Container, Link, Toolbar, Typography} from "@mui/material";
+import mailImg from "img/mail_orange.png";
 
 export function SignupForm() {
   const email = useAppSelector(selectEmail);
@@ -41,6 +42,38 @@ export function SignupForm() {
           </nav>
         </Toolbar>
       </AppBar>
+      <Container disableGutters maxWidth="sm" component="main" sx={{pt: 8, pb: 6}}>
+        <Typography
+          component="h1"
+          variant="h1"
+          align="center"
+          color="text.primary"
+          gutterBottom
+        >
+          <div><img src={mailImg}  alt="mail envelope"/></div>
+          Orangered
+        </Typography>
+        <Typography variant="h5" align="center" color="text.secondary" component="p">
+          The best content from your favorite subreddits delivered to your inbox
+        </Typography>
+      </Container>
+      <Container disableGutters maxWidth="sm" component="main" sx={{pt: 8, pb: 6}}>
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          color="text.primary"
+          gutterBottom
+        >
+          About
+        </Typography>
+        <Typography variant="h6" align="center" color="text.secondary" component="p">
+          Select your favorite subreddits and we'll send you a daily email with the top posts from each one.
+          <div>
+            <Link href="/static/html/sample_email.html" target="_blank">Click here to see a sample email.</Link>
+          </div>
+        </Typography>
+      </Container>
     </>
   );
 }
